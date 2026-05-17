93f40a69c5e7aaa1cd21db376e8c6e262bea80f4
ds asante <dsasante1@gmail.com>
2026-05-17 23:26:02 +0000

    fix: payout express pay value

--- src/utils/constants/shared.ts
+++ src/utils/constants/shared.ts
@@ -213,5 +213,5 @@ export const DEFAULT_QUERY = {
 export const PAYMENT_GATEWAY_OPTIONS = [
   { label: 'Paystack', value: 'paystack' },
   { label: 'Eganow', value: 'eganow' },
-  { label: 'ExpressPay', value: 'express_payout' },
+  { label: 'ExpressPay', value: 'expresspay' },
 ] as const;
